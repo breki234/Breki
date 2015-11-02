4fa68926b448d94c9f4b89721c1f2f7f6275d3d9
unknown <fs417452@STOFA-101-04.fss.is>
2015-11-02 15:40:12 +0000

    update2

--- planner.php
+++ planner.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
 <html>
 <body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script>
+console.log("hello");
+  for( var i=0; i<20; i++){
+    $.ajax("https:eune.api.pvp.net/api/lol/eune/v1.2/champion/57"+i+"?api_key=0195140c-442e-47a9-ad8b-1c9691befc4c")
+    .done(function(data){
+      document.innerHTML= document.innerHTML + data;
+    })
+  }
+</script>
 
-<?php
-$s =
-for i in https: eune.api.pvp.net/api/lol/eune/v1.2/champion/"57+1"?api_key=0195140c-442e-47a9-ad8b-1c9691befc4c
-?>
 
 </body>
 </html>
